875b02ee13b0de8fa4f86a237f61df33d8619bca
endhellopes <endheltog@gmail.com>
2020-04-18 18:18:45 -0300

    Página de criar lista

---
+++ lib/pages/newLista_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class NewListPage extends StatefulWidget {
+  @override
+  _NewListPageState createState() => _NewListPageState();
+}
+
+class _NewListPageState extends State<NewListPage> {
+  List<Map> _list = List();
+  final _productController = TextEditingController();
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(32),
+                      color: Colors.white,
+                    ),
+                    child: TextField(
+                      controller: _productController,
+                      decoration: InputDecoration(
+                        hintText: "Insira um produto",
+                        hintStyle: TextStyle(),
+                        border: InputBorder.none,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.add_circle,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _addProduct();
+                      _productController.text = "";
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 100,
+              child: Padding(
+                padding: EdgeInsets.only(top: 20, left: 60, right: 60),
+                child: ListView.builder(
+                  itemBuilder: buildItem,
+                  itemCount: _list.length,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _addProduct() {
+    Map product = {};
+    product["product"] = _productController.text;
+    product["checked"] = false;
+    _list.add(product);
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.transparent,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.transparent,
+        child: Align(
+          alignment: Alignment(0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      onDismissed: (_) {
+        setState(() {
+          _lastRemoved = Map.from(_list[index]);
+          _lastRemovedPos = index;
+          _list.removeAt(index);
+
+          final snack = SnackBar(
+            content: Text("Produto \"${_lastRemoved['product']}\" removido!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              textColor: Colors.pink,
+              onPressed: () {
+                setState(() {
+                  _list.insert(_lastRemovedPos, _lastRemoved);
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: Colors.white,
+        ),
+        child: CheckboxListTile(
+          checkColor: Colors.lightGreen,
+          activeColor: Colors.white,
+          dense: true,
+          controlAffinity: ListTileControlAffinity.leading,
+          value: _list[index]["checked"],
+          onChanged: (_) {
+            setState(() {
+              _list[index]["checked"] = !_list[index]["checked"];
+            });
+          },
+          title: Container(
+            width: 100,
+            child: Text(
+              _list[index]["product"],
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+          subtitle: TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Preço",
+              labelStyle: TextStyle(
+                color: Colors.white,
+              ),
+              border: InputBorder.none,
+            ),
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
