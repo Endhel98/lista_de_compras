7b420a3ae198faeacd6f1564c806cf2f1d5cb51f
endhellopes <endheltog@gmail.com>
2020-04-20 17:45:23 -0300

    Create functions.dart

---
+++ lib/functionsJson/functions.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+Future<File> getFile() async {
+  final directory = await getApplicationDocumentsDirectory();
+  return File("${directory.path}/data.json");
+}
+
+Future<String> readData() async {
+  try {
+    final file = await getFile();
+    return file.readAsString();
+  } catch (e) {
+    return null;
+  }
+}
+
+Future<File> saveData(List list) async {
+  String data = json.encode(list);
+  final file = await getFile();
+  return file.writeAsString(data);
+}
