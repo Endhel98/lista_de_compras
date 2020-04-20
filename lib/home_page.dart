dfaa458c5c13a7665c764b44b766857e76df03bd
endhellopes <endheltog@gmail.com>
2020-04-19 21:20:08 -0300

    AppBar adicionado

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,6 +17,78 @@ class _HomePageState extends State<HomePage> {
       length: 2,
       initialIndex: 1,
       child: Scaffold(
+        //extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: !_isSearching
+              ? Text(
+                  "Lista de Compras",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                )
+              : TextField(
+                  controller: _seachController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    hintText: "Insira o nome da Lista",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  ),
+                  style: TextStyle(fontSize: 18),
+                ),
+          actions: <Widget>[
+            !_isSearching
+                ? IconButton(
+                    icon: Icon(Icons.search),
+                    color: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        _isSearching = !_isSearching;
+                      });
+                    },
+                  )
+                : IconButton(
+                    icon: Icon(Icons.cancel),
+                    color: Colors.white,
+                    onPressed: () {
+                      setState(
+                        () {
+                          _isSearching = !_isSearching;
+                        },
+                      );
+                    },
+                  ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              color: Colors.white,
+              onPressed: () {},
+            )
+          ],
+          bottom: TabBar(
+            isScrollable: true,
+            labelPadding: EdgeInsets.symmetric(horizontal: 50.0),
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  "Criar Lista",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Listas",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
         body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -28,96 +100,10 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          child: Column(
+          child: TabBarView(
             children: <Widget>[
-              !_isSearching
-                  ? Padding(
-                      padding: EdgeInsets.only(top: 30, left: 30, bottom: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          Text(
-                            "Lista de Compras",
-                            style: TextStyle(
-                              fontSize: 26,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Icon(
-                            Icons.add_shopping_cart,
-                            color: Colors.white,
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.search),
-                            color: Colors.white,
-                            onPressed: () {
-                              setState(() {
-                                _isSearching = !_isSearching;
-                              });
-                            },
-                          )
-                        ],
-                      ),
-                    )
-                  : Padding(
-                      padding: EdgeInsets.only(top: 30, left: 10, bottom: 30),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              controller: _seachController,
-                              autofocus: true,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                border: InputBorder.none,
-                                hintText: "Insira o nome da Lista",
-                                hintStyle: TextStyle(color: Colors.grey),
-                                prefixIcon:
-                                    Icon(Icons.search, color: Colors.grey),
-                              ),
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.cancel),
-                            color: Colors.white,
-                            onPressed: () {
-                              setState(
-                                () {
-                                  _isSearching = !_isSearching;
-                                },
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-              TabBar(
-                isScrollable: true,
-                labelPadding: EdgeInsets.symmetric(horizontal: 50.0),
-                tabs: <Widget>[
-                  Tab(
-                    child: Text(
-                      "Criar Lista",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Tab(
-                    child: Text(
-                      "Listas",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: TabBarView(children: <Widget>[
-                  NewListPage(),
-                  ListsPage(),
-                ]),
-              )
+              NewListPage(),
+              ListsPage(),
             ],
           ),
         ),
