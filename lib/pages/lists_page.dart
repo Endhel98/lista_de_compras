d5a9489a11c267c9b023fe88624c0ba871527e03
endhellopes <endheltog@gmail.com>
2020-04-28 14:21:05 -0300

    listOfLists parâmetro para o ListsPage

--- lib/pages/lists_page.dart
+++ lib/pages/lists_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ListsPage extends StatelessWidget {
+  final List listOfLists;
+
+  ListsPage({this.listOfLists});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
+      body: ListView.builder(
+        itemCount: listOfLists.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(listOfLists[index]["name"]),
+          );
+        },
+      ),
     );
   }
 }
