ccd8c16109e699eaa487a4b955d649127a346f4b
endhellopes <endheltog@gmail.com>
2020-04-28 14:53:03 -0300

    Update lists_page.dart

--- lib/pages/lists_page.dart
+++ lib/pages/lists_page.dart
@@ -1,27 +1,48 @@
 import 'package:flutter/material.dart';
 
-class ListsPage extends StatelessWidget {
+class ListsPage extends StatefulWidget {
   final List listOfLists;
 
   ListsPage({this.listOfLists});
 
   @override
+  _ListsPageState createState() => _ListsPageState();
+}
+
+class _ListsPageState extends State<ListsPage> {
+  bool _arrow = false;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: ListView.builder(
-        itemCount: listOfLists.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              listOfLists[index]["name"],
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: widget.listOfLists.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                widget.listOfLists[index]["name"],
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
               ),
-            ),
-          );
-        },
+              trailing: IconButton(
+                icon: !_arrow
+                    ? Icon(Icons.arrow_drop_down)
+                    : Icon(Icons.arrow_drop_up),
+                color: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    _arrow = !_arrow;
+                  });
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
