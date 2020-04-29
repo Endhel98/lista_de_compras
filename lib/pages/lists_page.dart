68e45af7a7b65b8cc7cd576ffa957ea4c0ff5a2f
endhellopes <endheltog@gmail.com>
2020-04-29 16:38:27 -0300

    Remoção das listas

--- lib/pages/lists_page.dart
+++ lib/pages/lists_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lista_de_compras/functionsJson/functions.dart';
 
 class ListsPage extends StatefulWidget {
   final List listOfLists;
@@ -10,6 +11,14 @@ class ListsPage extends StatefulWidget {
 }
 
 class _ListsPageState extends State<ListsPage> {
+  List _list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _list = widget.listOfLists;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +26,26 @@ class _ListsPageState extends State<ListsPage> {
       body: Container(
         padding: EdgeInsets.all(20),
         child: ListView.separated(
-          itemCount: widget.listOfLists.length,
+          itemCount: _list.length,
           itemBuilder: (context, index) {
             return ListTile(
               title: Text(
-                widget.listOfLists[index]["name"],
+                _list[index]["name"],
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 18,
                 ),
               ),
               dense: true,
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                color: Colors.white,
+                onPressed: () {
+                  setState(() {
+                    _removeList(index);
+                  });
+                },
+              ),
             );
           },
           separatorBuilder: (_, __) {
@@ -36,5 +54,40 @@ class _ListsPageState extends State<ListsPage> {
         ),
       ),
     );
+  }
+
+  void _removeList(index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Tem certeza que deseja excluir a lista?"),
+          content: Text("Se sim, a lista será excluída permanentemente!"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancelar"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text("Sim"),
+              onPressed: () {
+                widget.listOfLists.removeAt(index);
+                saveData(widget.listOfLists);
+                _getAllLists();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _getAllLists() {
+    setState(() {
+      _list = widget.listOfLists;
+    });
   }
 }
