87cef80cb7d5314421cb039f79a771a2a0ff6936
endhellopes <endheltog@gmail.com>
2020-05-06 16:24:11 -0300

    Update functions.dart

--- lib/functionsJson/functions.dart
+++ lib/functionsJson/functions.dart
@@ -1,9 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:lista_de_compras/pages/newLista_page.dart';
 import 'package:path_provider/path_provider.dart';
-
-List<Map> newList = List.from(shoppingCart);
 
 Future<File> getFile() async {
   final directory = await getApplicationDocumentsDirectory();
