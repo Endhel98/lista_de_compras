76bee84a92e51a1289d301edc048fc192afbd42e
endhellopes <endheltog@gmail.com>
2020-05-06 15:20:19 -0300

    TabBar deletado

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:lista_de_compras/functionsJson/functions.dart';
-import 'package:lista_de_compras/pages/lists_page.dart';
 import 'package:lista_de_compras/pages/newLista_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,8 +11,6 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   final _searchController = TextEditingController();
   bool _isSearching = false;
-  List _list = [];
-  TabController controller;
 
   void _filterList(String value) {
     setState(() {
@@ -25,23 +21,6 @@ class _HomePageState extends State<HomePage>
               .contains(value.toLowerCase()))
           .toList();
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    controller = new TabController(vsync: this, length: 2);
-    readData().then((data) {
-      setState(() {
-        _list = json.decode(data);
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.dispose();
   }
 
   @override
@@ -66,7 +45,7 @@ class _HomePageState extends State<HomePage>
                   filled: true,
                   fillColor: Colors.white,
                   border: InputBorder.none,
-                  hintText: "Insira o nome da Lista",
+                  hintText: "Insira o nome do produto",
                   hintStyle: TextStyle(color: Colors.grey),
                   prefixIcon: Icon(Icons.search, color: Colors.grey),
                 ),
@@ -102,51 +81,19 @@ class _HomePageState extends State<HomePage>
                   },
                 ),
         ],
-        bottom: TabBar(
-          controller: controller,
-          isScrollable: true,
-          labelPadding: EdgeInsets.symmetric(horizontal: 50.0),
-          tabs: <Widget>[
-            Tab(
-              child: Text(
-                "Criar Lista",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            Tab(
-              child: Text(
-                "Listas",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
-        ),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            colors: [
-              Colors.pink[400],
-              Colors.pink[300],
-              Colors.pink[200],
-            ],
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              colors: [
+                Colors.pink[400],
+                Colors.pink[300],
+                Colors.pink[200],
+              ],
+            ),
           ),
-        ),
-        child: TabBarView(
-          controller: controller,
-          children: <Widget>[
-            NewListPage(
-              list: _list,
-              isSearching: _isSearching,
-            ),
-            ListsPage(
-              list: _list,
-              //controller: controller,
-            ),
-          ],
-        ),
-      ),
+          child: null),
     );
   }
 }
