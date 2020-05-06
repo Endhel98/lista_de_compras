5db9a025f4a45c2257c5c414d37b14479f0fc3a5
endhellopes <endheltog@gmail.com>
2020-05-06 15:34:57 -0300

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   final _searchController = TextEditingController();
+  final _productController = TextEditingController();
+  //final _listNameController = TextEditingController();
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
   bool _isSearching = false;
 
   void _filterList(String value) {
@@ -21,6 +26,13 @@ class _HomePageState extends State<HomePage>
               .contains(value.toLowerCase()))
           .toList();
     });
+  }
+
+  void _addProduct() {
+    Map product = {};
+    product["product"] = _productController.text;
+    product["checked"] = false;
+    shoppingCart.add(product);
   }
 
   @override
@@ -83,17 +95,146 @@ class _HomePageState extends State<HomePage>
         ],
       ),
       body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              colors: [
-                Colors.pink[400],
-                Colors.pink[300],
-                Colors.pink[200],
-              ],
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            colors: [
+              Colors.pink[400],
+              Colors.pink[300],
+              Colors.pink[200],
+            ],
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(32),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        controller: _productController,
+                        decoration: InputDecoration(
+                          hintText: "Insira um produto",
+                          hintStyle: TextStyle(),
+                          border: InputBorder.none,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.add_circle,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _addProduct();
+                        _productController.text = "";
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                height: 100,
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(top: 20, left: 60, right: 60, bottom: 70),
+                  child: ListView.builder(
+                    itemBuilder: buildItem,
+                    itemCount:
+                        _isSearching ? newList.length : shoppingCart.length,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.transparent,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.transparent,
+        child: Align(
+          alignment: Alignment(0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      onDismissed: (_) {
+        setState(() {
+          _lastRemoved = Map.from(shoppingCart[index]);
+          _lastRemovedPos = index;
+          shoppingCart.removeAt(index);
+
+          final snack = SnackBar(
+            content: Text("Produto \"${_lastRemoved['product']}\" removido!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              textColor: Colors.pink,
+              onPressed: () {
+                setState(() {
+                  shoppingCart.insert(_lastRemovedPos, _lastRemoved);
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: Colors.white,
+        ),
+        child: CheckboxListTile(
+          checkColor: Colors.lightGreen,
+          activeColor: Colors.white,
+          dense: true,
+          controlAffinity: ListTileControlAffinity.leading,
+          value: _isSearching
+              ? newList[index]["checked"]
+              : shoppingCart[index]["checked"],
+          onChanged: (_) {
+            setState(() {
+              newList[index]["checked"] = !newList[index]["checked"];
+            });
+          },
+          title: Container(
+            width: 100,
+            child: Text(
+              _isSearching
+                  ? newList[index]["product"]
+                  : shoppingCart[index]["product"],
+              style: TextStyle(fontSize: 18, color: Colors.white),
             ),
           ),
-          child: null),
+        ),
+      ),
     );
   }
 }
