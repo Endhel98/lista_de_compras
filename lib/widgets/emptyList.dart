69536b138fa7d0f2907c15f6e346deb96c25c4e4
endhel <endheltog@gmail.com>
2020-08-06 18:49:10 -0300

    Bug Resolved

--- lib/widgets/emptyList.dart
+++ lib/widgets/emptyList.dart
@@ -4,23 +4,25 @@ class EmptyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView(
-        padding: EdgeInsets.all(115.0),
-        children: <Widget>[
-          Icon(
-            Icons.shopping_cart,
-            size: 50.0,
-            color: Colors.white,
-          ),
-          Text(
-            "Carrinho vazio :(",
-            style: TextStyle(
+      child: Container(
+        margin: EdgeInsets.only(top: 100.0),
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.shopping_cart,
+              size: 50.0,
               color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16.0,
             ),
-          ),
-        ],
+            Text(
+              "Carrinho vazio :(",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
